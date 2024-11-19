ab4fae774bb1efc9167cfbe631db838a659b6121
Raj Kumar <raj@example.com>
2024-11-19 13:08:29 +0530

    python

---
+++ table.py
@@ -0,0 +1,17 @@
+## print the multiplication table of a number n.(using while)
+n =3
+i = 1
+while i <=10:
+     print("table of :", n*i)
+     i +=1
+
+## print the multiplication table of a number n.
+for i in range(2,22,2):
+     print(i)
+    # or
+n = int(input("enter number :"))
+
+for i in range(1,11):
+     print(n*i)
+
+
