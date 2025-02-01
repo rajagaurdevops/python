17dfc0c544f6c6d77f8f9ec2f3f65172f4277066
Raj Kumar <raj@example.com>
2025-02-01 14:30:51 +0530

    modified file

--- if_elif_else.py
+++ if_elif_else.py
@@ -45,3 +45,40 @@ if (x > 5):
 # output
     above 5
  but not above 20
+
+
+
+# wap to check if a number entrerd by the user is odd or even
+
+number = int(input("enter your number :"))
+
+if(number % 2 == 0):
+    print("even")
+else:
+    print("odd")
+
+
+
+# wap to find the greatest of 3 number entered by the user
+
+num1 = int(input("enter first number :"))
+num2 = int(input("enter second number :"))
+num3 = int(input("enter third number :"))
+
+if(num1 > num2 and num1 > num3):
+    print("first number is largest", num1)
+elif(num2 > num1 and num2 > num3):
+    print("second number is largest ", num2)
+else:
+    print("third number is largest", num3)
+
+
+
+# wap to check if a number is a multiply of 7 or not
+
+a = int(input("enter your number :"))
+
+if(a % 7 == 0):
+   print("multiply of 7 :", a)
+else:
+   print("not multiply of 7 :", a)
